847471ec993621440646c10444f3a0e9798944df
RODRIIGOLDEN <rodrigotoyo@gmail.com>
2023-08-07 20:09:13 -0500

    Agregar archivos de proyecto.

---
+++ hola repo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola_repo
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO");
+        }
+    }
+}
